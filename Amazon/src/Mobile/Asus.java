5b3064f8a79f444dc1ebb3f8eff371b68516f641
rajavelmechengg <100897025+rajavelmechengg@users.noreply.github.com>
2022-03-09 20:27:57 +0530

    Delete Amazon directory

--- Amazon/src/Mobile/Asus.java
+++
@@ -1,9 +0,0 @@
-package Mobile;
-
-public class Asus {
-
-	public static void main(String[] args) {
-		System.out.println("ZENFONE 5Z");
-	}
-}
-
--- Amazon/src/Mobile/Iphone.java
+++
@@ -1,7 +0,0 @@
-package Mobile;
-
-public class Iphone {
-public static void main(String[] args) {
-	System.out.println("IPHONE 13PRO");
-}
-}
